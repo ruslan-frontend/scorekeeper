9cceb5fe3060c8b3f2eb62a007624867155c19b2
Ruslan <ruslan.bulatov.96@gmail.com>
2023-01-23 21:48:16 +0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { FormControl } from '@mui/material';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <FormControl>
+  <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="female"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="female" control={<Radio />} label="Female" />
+    <FormControlLabel value="male" control={<Radio />} label="Male" />
+    <FormControlLabel value="other" control={<Radio />} label="Other" />
+  </RadioGroup>
+</FormControl>
+      </header>
+    </div>
+  );
+}
+
+export default App;
